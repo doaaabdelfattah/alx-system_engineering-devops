814bfc54f7e6ac8f672dc092332b0c9b828ce534
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-23 23:32:23 +0200

    “update”

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create file in tmp
 
-file { '/tmp/school':
+file { 'school':
 ensure => present,
 content => 'I love Puppet',
+path => '/tmp/'
 mode => '0744',
 owner => 'www-data',
 group => 'www-data'
