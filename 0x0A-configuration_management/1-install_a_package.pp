5ace4d468b3605cbb5d37cd8782e910324fdb689
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-23 23:46:16 +0200

    “update”

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,4 @@
 # Using Puppet, install flask from pip3.
-
-package { 'Python':
-  ensure   => '3.8.10',
-  provider => 'pip3',
-}
-
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
