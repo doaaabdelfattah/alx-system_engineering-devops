b4f8c8209385a6ac372298000112d1102c7b9669
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-25 12:35:01 +0200

    “task_0”

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#using Puppet to make changes to our configuration file.
+file { '~/.ssh/school':
+    ensure => present,
+    mode => '600',
+    content => "\
+    Host 501006-web-01
+        HostName 54.237.109.172
+        User ubuntu
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+    ",
+}
