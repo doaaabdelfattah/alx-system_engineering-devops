f1f86b8222ea693ba36bc4b9b831b18993f68b44
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-02-28 23:12:48 +0200

    “advanced”

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+#Install NGINX
+package { 'nginx':
+    provider => 'apt',
+}
+
+# Main file for string
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+  mode    => '0744',
+}
+
+
+# 301 Redirection file 
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => present,
+  mode    => '600',
+  content => "\
+server {
+  listen 80;
+  listen [::]:80 default_server;
+  root   /etc/nginx/html;
+  index  index.html index.htm;
+
+  location /redirect_me {
+    return 301 http://youtube.com/;
+  }
+}
+",
+}
+
+# Restart NGINX 
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-enabled/default'],
+}
