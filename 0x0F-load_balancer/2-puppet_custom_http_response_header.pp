027e3ce6abc13baa95ea7d7b63b8d577ec2281bd
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-10 12:09:45 +0200

    “UPDATE”

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,52 +1,25 @@
-# Install NGINX
-package { 'nginx':
-  ensure => 'installed',
+# Installs a Nginx server with custom HTTP header
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# Create directory to store HTML files
-file { '/etc/nginx/html':
-  ensure => directory,
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# Create default index.html page
-file { '/etc/nginx/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# 301 Redirection file
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  mode    => '600',
-  content => "\
-server {
-  listen 80;
-  listen [::]:80 default_server;
-  root   /etc/nginx/html;
-  index  index.html index.htm;
-
-  location /redirect_me {
-    return 301 http://youtube.com/;
-  }
-
-  # Add custom HTTP header X-Served-By with the hostname of the server
-  add_header X-Served-By \$hostname;
-
-}
-",
-}
-
-# Enable the redirection configuration using nginx::resource::vhost
-nginx::resource::vhost { 'default':
-  ensure => present,
-}
-
-# Restart NGINX
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
