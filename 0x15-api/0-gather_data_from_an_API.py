9c1ce192c4e602e3be3f22f5a07aaa77072b4b28
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-03-26 03:01:25 +0200

    “n”

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
+'''Python script that
+returns information about his todo list progress.
 '''
-return todo list info from fiven id from API
-'''
-import requests
-import sys
-
-
 if __name__ == "__main__":
-    user_url = "https://jsonplaceholder.typicode.com/"
-    user_id = int(sys.argv[1])
+    import requests
+    from sys import argv
 
-    # Make a GET request to fetch user information
-    response_user = requests.get(f'{user_url}/users/{user_id}')
-    # Extracts the JSON content from the HTTP response
-    # It return a Python dict that can be accessed
-    user_name = response_user.json()['name']
+    urlUser = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    response_name = requests.get(urlUser)
+    name = response_name.json()['name']
 
-    user_todo = 'https://jsonplaceholder.typicode.com/todos'
-
-    # Make a GET request to fetch user's todo list
-    response_Todo = requests.get(f'{user_todo}?userId={user_id}')
-    user_tasks = response_Todo.json()
+    urlTodo = urlUser + '/todos'
+    response_todos = requests.get(urlTodo)
+    tasks = response_todos.json()
+    total_task = len(tasks)
 
     count = 0
-    for data in user_tasks:
-        if data['completed'] is True:
+    for task in tasks:
+        if task['completed'] is True:
             count += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(user_name,
-          count, len(user_tasks)))
+    print("Employee {} is done with tasks({}/{}):".format(name, count,
+          total_task))
 
-    for task in user_tasks:
+    for task in tasks:
         if task['completed'] is True:
-            print("\t {}".format(task['title']))
+            task_name = task['title']
+            print("\t {}".format(task_name))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+return todo list info from fiven id from API
+'''
+import requests
+import csv
+import sys
+
+if __name__ == "__main__":
+    
+    user_url = "https://jsonplaceholder.typicode.com/"
+    user_id = int(sys.argv[1])
+
+    # Make a GET request to fetch user information
+    response_user = requests.get(f'{user_url}/users/{user_id}')
+    # Extracts the JSON content from the HTTP response
+    # It return a Python dict that can be accessed
+    user_name = response_user.json()['name']
+
+    user_todo = 'https://jsonplaceholder.typicode.com/todos'
+
+    # Make a GET request to fetch user's todo list
+    response_Todo = requests.get(f'{user_todo}?userId={user_id}')
+    user_tasks = response_Todo.json()
+    
+    # Writing result to CSV file
+    csv_filename = f"{user_name}_todo.csv"
+    with open(csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Title', 'Completed'])
+        for task in completed_tasks:
+            writer.writerow([task['title'], 'Yes'])
