cb7c34152ec2bbaa6915f226173321b0207382aa
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-04-12 04:05:47 +0200

    “task2”

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-'''
-returns the number of subscribers
-'''
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    ''' returns the number of subscribers
-    '''
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    # Make a GET request to fetch subreddit information
-    response = requests.get(url, params={"after": after},
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
-        # If subreddit not found, return 0 subscribers
         return None
 
-    # Extract JSON content from the response
-    data = response.json()
-    #  extracts the value of the "after" key from JSON
-    after = data.get('after')
-    # Extract the list of posts from the JSON data
-    list_data = data['data']
-    # Print titles of the top ten posts
-    for post in list_data['children']:
-        hot_list.append(post.get('data').get('title'))
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-        return recurse(subreddit, hot_list, after)
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
