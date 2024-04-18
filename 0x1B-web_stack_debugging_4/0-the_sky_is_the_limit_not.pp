56a5eacbdb9d5439a0afa50ae09dcf144443c646
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-04-18 10:17:04 +0200

    “task0”

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increase traffic of Nginx
+
+# Increase the LIMIT of the default file
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
